f8c3a809dd3aa66ac360ef130bb63e61b2687ea0
Jake Bethke <jbethke@ufl.edu>
2024-11-14 15:46:53 -0500

    This is the full whackamole code

--- whackamole.py
+++ whackamole.py
@@ -1,20 +1,60 @@
 import pygame
+import sys
+import random
 
+def initialize_board():
+    return [["-" for i in range(640)] for j in range(512)]
 
+pygame.init()
+screen = pygame.display.set_mode((640, 512))
+board = initialize_board()
+screen.fill("pink")
+
+def draw_grid():
+    for i in range(1,16):
+        pygame.draw.line(
+            screen,
+            (0,0,0),
+            (0,i*32),
+            (640,i*32),
+            1
+        )
+
+    for i in range(1,20):
+        pygame.draw.line(
+            screen,
+            (0, 0, 0),
+            (i*32,0),
+            (i*32,512),
+            1
+        )
+draw_grid()
 def main():
     try:
         pygame.init()
+        x=4
+        y=4
         # You can draw the mole with this snippet:
-        # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
         mole_image = pygame.image.load("mole.png")
-        screen = pygame.display.set_mode((640, 512))
+        screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
         clock = pygame.time.Clock()
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-            screen.fill("light green")
+                area = pygame.Rect(x,y,32,32)
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if area.collidepoint(event.pos):
+                        screen.fill("pink")
+                        draw_grid()
+                        row =random.randrange(1,640)
+                        col =random.randrange(1,512)
+                        x=(row//32)*32
+                        y=(col//32)*32
+                        screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
+
+
             pygame.display.flip()
             clock.tick(60)
     finally:
